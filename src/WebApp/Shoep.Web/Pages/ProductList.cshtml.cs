412999a9ce9518f9e97d2e57eac7e563dbf433b0
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-10-21 03:16:45 +0700

    Add theme - ui client

---
+++ src/WebApp/Shoep.Web/Pages/ProductList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shoep.Web.Pages
+{
+    public class ProductListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
