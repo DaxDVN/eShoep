5099c30bddb63a7768553b7fb8d1c7462973b62a
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-10-21 20:53:44 +0700

    Add CQRD endpoint - handler

---
+++ src/Services/Catalog/Catalog.API/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Models
+{
+  public class Category
+  {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public int? ParentCategoryId { get; set; }
+  }
+}
---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Models
+{
+  public class Product
+  {
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public int CategoryId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+  }
+}
