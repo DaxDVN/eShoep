e3a5a1c58b9881fab22a8ef6f46d1a4b97ad1173
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-10-24 01:24:55 +0700

    Add identity

---
+++ src/Services/Identity/Identity.API/Config.cs
@@ -0,0 +1,99 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Identity.API
+{
+  public static class Config
+  {
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+              new IdentityResources.OpenId(),
+              new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiResource> GetApis()
+    {
+      return new List<ApiResource>
+            {
+                new ApiResource("orders", "Orders Service"),
+                new ApiResource("basket", "Basket Service"),
+                new ApiResource("webhooks", "Webhooks registration Service"),
+            };
+    }
+
+    public static IEnumerable<ApiScope> GetApiScopes()
+    {
+      return new List<ApiScope>
+            {
+                new ApiScope("orders", "Orders Service"),
+                new ApiScope("basket", "Basket Service"),
+                new ApiScope("webhooks", "Webhooks registration Service"),
+            };
+    }
+    public static IEnumerable<Client> Clients(IConfiguration config) =>
+        new Client[]
+        {
+            // m2m client credentials flow client
+            new Client
+            {
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = {"openid", "profile", "eShoep"},
+                RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
+                ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
+                AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "razorApp",
+                ClientName = "Razor Page eCommerce App",
+                ClientSecrets = {new Secret("razor_secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+                RedirectUris = { config["ClientApp"] + "/signin-oidc" },
+                PostLogoutRedirectUris = { config["ClientApp"] + "/signout-callback-oidc" },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "eShoep" },
+                AccessTokenLifetime = 3600 * 24 * 30, // 30 days
+                AlwaysIncludeUserClaimsInIdToken = true
+            },
+            new Client
+            {
+                ClientId = "webapp",
+                ClientName = "WebApp Client",
+                ClientSecrets = new List<Secret>
+                {
+                    new Secret("secret".Sha256())
+                },
+                ClientUri = $"{config["WebAppClient"]}",
+                AllowedGrantTypes = GrantTypes.Code,
+                AllowAccessTokensViaBrowser = false,
+                RequireConsent = false,
+                AllowOfflineAccess = true,
+                AlwaysIncludeUserClaimsInIdToken = true,
+                RequirePkce = false,
+                RedirectUris = new List<string>
+                {
+                    $"{config["WebAppClient"]}/signin-oidc"
+                },
+                PostLogoutRedirectUris = new List<string>
+                {
+                    $"{config["WebAppClient"]}/signout-callback-oidc"
+                },
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.OfflineAccess,
+                    "orders",
+                    "basket",
+                    "webshoppingagg",
+                    "webhooks"
+                },
+                AccessTokenLifetime = 60*60*2, // 2 hours
+                IdentityTokenLifetime= 60*60*2 // 2 hours
+            },
+        };
+  }
+}
---
+++ src/Services/Identity/Identity.API/HostingExtensions.cs
@@ -0,0 +1,79 @@
+using Identity.API.Data;
+using Identity.API.Models;
+using Identity.API.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace Identity.API
+{
+  internal static class HostingExtensions
+  {
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+      builder.Services.AddRazorPages();
+
+      builder.Services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<ApplicationDbContext>()
+          .AddDefaultTokenProviders();
+
+      builder.Services
+            .AddIdentityServer(options =>
+            {
+              options.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+
+              options.Events.RaiseErrorEvents = true;
+              options.Events.RaiseInformationEvents = true;
+              options.Events.RaiseFailureEvents = true;
+              options.Events.RaiseSuccessEvents = true;
+              //options.IssuerUri = builder.Configuration["IssuerUri"];
+
+              // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+              // options.EmitStaticAudienceClaim = true;
+            })
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.GetApiScopes())
+            .AddInMemoryApiResources(Config.GetApis())
+            .AddInMemoryClients(Config.Clients(builder.Configuration))
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddProfileService<CustomProfileService>();
+
+      builder.Services.ConfigureApplicationCookie(options =>
+      {
+        options.Cookie.SameSite = SameSiteMode.Lax;
+      });
+
+      builder.Services.ConfigureExternalCookie(options =>
+      {
+        options.Cookie.SameSite = SameSiteMode.Lax;
+      });
+
+      builder.Services.AddAuthentication();
+
+      return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+      app.UseSerilogRequestLogging();
+
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseStaticFiles();
+      app.UseRouting();
+      app.UseIdentityServer();
+      app.UseAuthorization();
+
+      app.MapRazorPages()
+          .RequireAuthorization();
+
+      return app;
+    }
+  }
+}
