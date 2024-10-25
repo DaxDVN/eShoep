2e37dcee1946fdda4e2945705852097b80f2eb9e
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-10-25 20:15:01 +0700

    Add order purchasing

---
+++ src/Services/Purchasing/Purchasing.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Purchasing.Domain.Abstractions;
+global using Purchasing.Domain.Models;
+global using Purchasing.Domain.ValueObjects;
+global using Purchasing.Infrastructure.Data;
+global using Purchasing.Infrastructure.Data.Interceptors;
