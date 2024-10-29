b9069fba563906e5acb4698238cd12eb415b58dc
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-10-30 04:46:45 +0700

    Update Identity

---
+++ src/Services/Identity/Identity.API/Program.cs
@@ -0,0 +1,36 @@
+using Identity.API.Data;
+using Identity.API.Entities;
+using Identity.API.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme)
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
+
+app.Run();
