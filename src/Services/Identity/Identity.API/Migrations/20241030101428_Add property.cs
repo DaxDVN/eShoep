07ac5a21aa3dd5183825ed75eae5abfb44b90fff
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-10-30 18:16:16 +0700

    Add property

---
+++ src/Services/Identity/Identity.API/Migrations/20241030101428_Add property.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Initials",
+                schema: "identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                schema: "identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Initials",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(5)",
+                maxLength: 5,
+                nullable: true);
+        }
+    }
+}
