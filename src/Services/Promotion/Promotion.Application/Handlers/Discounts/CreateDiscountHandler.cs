db65dd10a98a4676c7a73881cea8268d2df307a5
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-11-02 03:50:32 +0700

    Add feature create discount service

---
+++ src/Services/Promotion/Promotion.Application/Handlers/Discounts/CreateDiscountHandler.cs
@@ -0,0 +1,55 @@
+﻿namespace Promotion.Application.Handlers.Discounts;
+
+public record CreateDiscountCommand(
+    string Name,
+    string PromotionType,
+    decimal Amount,
+    DateTime StartDate,
+    DateTime EndDate,
+    List<Guid> ProductIds,
+    bool IsActive)
+    : ICommand<CreateDiscountResult>;
+
+public record CreateDiscountResult(Guid Id);
+
+public class CreateDiscountCommandValidator : AbstractValidator<CreateDiscountCommand>
+{
+    public CreateDiscountCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Discount Name is required.")
+            .Matches(@"^[A-Z0-9]+$").WithMessage("Discount Name must be alphanumeric and uppercase.");
+
+        RuleFor(x => x.PromotionType)
+            .NotEmpty().WithMessage("PromotionType is required.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than 0.");
+
+        RuleFor(x => x.ProductIds)
+            .NotNull().WithMessage("Product ID list cannot be null.");
+
+        RuleFor(x => x.IsActive)
+            .NotNull().WithMessage("IsActive must be specified.");
+
+        RuleFor(x => x.StartDate)
+            .GreaterThan(DateTime.UtcNow).WithMessage("StartDate must be in the future.");
+
+        RuleFor(x => x.EndDate)
+            .GreaterThan(DateTime.UtcNow).WithMessage("EndDate must be in the future.");
+    }
+}
+
+internal class CreateDiscountCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateDiscountCommand, CreateDiscountResult>
+{
+    public async Task<CreateDiscountResult> Handle(CreateDiscountCommand command, CancellationToken cancellationToken)
+    {
+        var discount = command.Adapt<Discount>();
+        session.Store(discount);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateDiscountResult(discount.Id);
+    }
+}
---
+++ src/Services/Promotion/Promotion.Domain/Models/Discount.cs
@@ -0,0 +1,13 @@
+﻿namespace Promotion.Domain.Models;
+
+public class Discount
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public PromotionType PromotionType { get; set; } = default!;
+    public decimal Amount { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public List<Guid> ProductIds { get; set; }
+    public bool IsActive { get; set; }
+}
